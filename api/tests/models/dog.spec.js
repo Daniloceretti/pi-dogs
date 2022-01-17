69faa65bf0f903a2841b49e9e0cc963bec904d06
Danilo ceretti <88346919+Daniloceretti@users.noreply.github.com>
2022-01-17 19:45:14 -0300

    Add files via upload

---
+++ api/tests/models/dog.spec.js
@@ -0,0 +1,37 @@
+const { Dog, conn,Temperament } = require('../../src/db.js');
+const { expect } = require('chai');
+
+describe('Dog model', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators', () => {
+    beforeEach(() => Dog.sync({ force: true }));
+    describe('name', () => {
+      it('should throw an error if name is null', (done) => {
+        Dog.create({})
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
+      });
+      it('should work when its a valid name', () => {
+        Dog.create({ name: 'Pug' });
+      });
+    });
+  });
+});
+describe('Temperament model', function () {
+  beforeEach(async function() {
+    await Temperament.sync({ force: false});
+  });
+  it('should throw an error if name is null', (done) => {
+    Temperament.create({id: "133dasdd3434"})
+      .then(() => done(new Error('It requires a valid name')))
+      .catch(() => done());
+  });
+  it('Name dshould be a STRING', function(){
+    expect(typeof Temperament.name).equal("string")
+  })
+
+  });
+
---
+++ api/tests/routes/dog.spec.js
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { expect } = require('chai');
+const session = require('supertest-session');
+const app = require('../../src/app.js');
+const { Dog, conn } = require('../../src/db.js');
+const request = require ('supertest')
+
+const agent = session(app);
+const dog = {
+ // id: "c27ef91-0e8b-4fda-a041-0c8e20104587",
+  name: 'Kim',
+  minheight: 20,
+  maxheight: 25,
+  minweight: 10,
+  maxweight: 18,
+  life_span: 13
+};
+
+describe('Dogs routes', () => {
+  before(() => conn.authenticate()
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  }));
+  beforeEach(() => Dog.sync({ force: false })
+    .then(() => Dog.create(dog)));
+  describe('GET /dogs', () => {
+    it('should get 200', () =>
+      agent.get('/dogs').expect(200)
+    );
+  });
+});
+
+describe('Dogs Routes', function() {
+  it('GET responds with a status 200 if it finds a dog', function() {
+    return agent 
+      .get('/dogs?name=Pug') 
+      .expect(function(res){
+        expect(res.status).equal(200)}); 
+  });
+})
+
+describe("Dogs Routes", function() {
+  it('GET responds with a status 200 if it finds a dog for id',  function() {
+    return agent 
+      .get('/dogs/1') 
+      .expect(function(res){
+        expect(res.status).equal(200)}); 
+  })
+})
+
